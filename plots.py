819202154ee8dd4a1bbddfaf07269dfbe3637f47
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2017-10-17 09:59:32 -0400

    Code cleanup

--- plots.py
+++ plots.py
@@ -1,7 +1,6 @@
 #!/usr/local/bin/python3
 
 from ROOT import TChain, TTree, TFile, TVector3
-from array import array
 from glob import glob
 import math
 from bdt_common import total_pot, variables, spectators
@@ -9,16 +8,16 @@ from bdt_common import x_start, x_end, y_start, y_end, z_start, z_end
 
 
 def is_fiducial(point):
-    ok_y = point[1] > y_start + 20 and point[1] < y_end - 20
-    ok_x = point[0] > x_start + 10 and point[0] < x_end - 10
-    ok_z = point[2] > z_start + 10 and point[2] < z_end - 50
+    ok_y = y_start + 20 < point[1] < y_end - 20
+    ok_x = x_start + 10 < point[0] < x_end - 10
+    ok_z = z_start + 10 < point[2] < z_end - 50
     return ok_y and ok_x and ok_z
 
 
 def is_active(point):
-    ok_y = point[1] > y_start and point[1] < y_end
-    ok_x = point[0] > x_start and point[0] < x_end
-    ok_z = point[2] > z_start and point[2] < z_end
+    ok_y = y_start < point[1] < y_end
+    ok_x = x_start < point[0] < x_end
+    ok_z = z_start < point[2] < z_end
     return ok_y and ok_x and ok_z
 
 
@@ -31,6 +30,16 @@ def choose_shower(root_chain):
                 most_energetic_shower = root_chain.shower_energy[ish]
                 shower_id = ish
     return shower_id
+
+def choose_track(root_chain):
+    track_id = 0
+    least_proton_track = 1
+
+    for itrk in range(root_chain.n_tracks):
+        if root_chain.predict_p[itrk] < least_proton_track:
+            least_proton_track = root_chain.predict_p[itrk]
+            track_id = itrk
+    return track_id
 
 
 def fill_kin_branches(root_chain, numu_chain, weight, variables):
@@ -62,7 +71,7 @@ def fill_kin_branches(root_chain, numu_chain, weight, variables):
         v_shower) / (v_track.Mag() * v_shower.Mag())
 
     signal = 0
-    if root_chain.category == 2:
+    if root_chain.category == 2 and root_chain.E < 1:
         signal = 1
 
     track_vertex = [
@@ -100,9 +109,8 @@ def fill_kin_branches(root_chain, numu_chain, weight, variables):
         direction = -1
     theta = math.acos(direction * root_chain.track_dir_z[track_id])
 
-    variables["dedx_hits"] = array("f", [1, 2, 3])
     variables["is_signal"][0] = signal
-    variables["track_length"][0] = root_chain.track_energy[track_id]
+    variables["track_length"][0] = root_chain.track_len[track_id]
     variables["track_phi"][0] = math.degrees(
         root_chain.track_phi[track_id])
     variables["track_theta"][0] = math.degrees(theta)
@@ -124,13 +132,13 @@ def fill_kin_branches(root_chain, numu_chain, weight, variables):
     variables["track_end_z"][0] = root_chain.track_end_z[track_id]
 
     variables["shower_start_x"][0] = root_chain.shower_start_x[shower_id]
-    #variables["shower_end_x"][0] = root_chain.shower_end_x[shower_id]
+    # variables["shower_end_x"][0] = root_chain.shower_end_x[shower_id]
 
     variables["shower_start_y"][0] = root_chain.shower_start_y[shower_id]
-    #variables["shower_end_y"][0] = root_chain.shower_end_y[shower_id]
+    # variables["shower_end_y"][0] = root_chain.shower_end_y[shower_id]
 
     variables["shower_start_z"][0] = root_chain.shower_start_z[shower_id]
-    #variables["shower_end_z"][0] = root_chain.shower_end_z[shower_id]
+    # variables["shower_end_z"][0] = root_chain.shower_end_z[shower_id]
 
     variables["reco_energy"][0] = root_chain.E
     variables["category"][0] = root_chain.category
@@ -144,46 +152,20 @@ def fill_kin_branches(root_chain, numu_chain, weight, variables):
     variables["event"][0] = root_chain.event
     variables["run"][0] = root_chain.run
     variables["subrun"][0] = root_chain.subrun
-    variables["proton_score"][0] = root_chain.predict_p[track_id]
+    variables["proton_score"][0] = max(0, root_chain.predict_p[track_id])
     variables["interaction_type"][0] = root_chain.interaction_type
 
     variables["shower_open_angle"][0] = math.degrees(
         root_chain.shower_open_angle[shower_id])
 
-    variables["numu_score"][0] = numu_selection(numu_chain)
+    # if numu_selection(numu_chain) < 1 and numu_selection(numu_chain) > 0:
+    #     variables["numu_score"][0] = numu_selection(numu_chain)
+    # else:
+    #     variables["numu_score"][0] = 0
 
     dedx = root_chain.shower_dEdx[shower_id][2]
 
-    if dedx < 0:
-        dedx = 0
-    variables["dedx"][0] = dedx
-
-
-def costheta_plot(root_chain, weight=1):
-    electrons = sum(1 for i, pdg in
-                    enumerate(root_chain.nu_daughters_pdg) if abs(pdg) == 11)
-
-    if electrons == 1:
-        e_dir = []
-
-        for i, pdg in enumerate(root_chain.nu_daughters_pdg):
-            if abs(pdg) == 11:
-                p = math.sqrt(
-                    root_chain.nu_daughters_px[i]**2 +
-                    root_chain.nu_daughters_py[i]**2 +
-                    root_chain.nu_daughters_pz[i]**2)
-                e_dir.append([root_chain.nu_daughters_px[i] / p,
-                              root_chain.nu_daughters_py[i] / p,
-                              root_chain.nu_daughters_pz[i] / p])
-
-        for ish in range(root_chain.n_showers):
-            v_reco = TVector3(
-                root_chain.shower_dir_x[ish],
-                root_chain.shower_dir_y[ish],
-                root_chain.shower_dir_z[ish])
-            v_true = TVector3(e_dir[0][0], e_dir[0][1], e_dir[0][2])
-            costheta = v_reco.Dot(v_true) / (v_reco.Mag() * v_true.Mag())
-            h_costhetas[root_chain.category].Fill(costheta, weight)
+    variables["dedx"][0] = max(0, dedx)
 
 
 def pt_plot(root_chain):
@@ -194,7 +176,7 @@ def pt_plot(root_chain):
                 root_chain.shower_dir_x[ish],
                 root_chain.shower_dir_y[ish],
                 root_chain.shower_dir_z[ish])
-            if (root_chain.shower_energy[ish] < 10):
+            if root_chain.shower_energy[ish] < 10:
                 p_vector.SetMag(
                     math.sqrt(
                         (root_chain.shower_energy[ish] + 0.052)**2 - 0.052**2))
@@ -231,6 +213,7 @@ def pt_plot(root_chain):
 
 
 def numu_selection(mychain):
+    if mychain.GetEntries() == 0: return 0
     for i in range(mychain.nslices):
         flashmatch_cut = not (
             mychain.slc_flsmatch_qllx[i] -
@@ -244,9 +227,13 @@ def numu_selection(mychain):
             mychain.slc_vtxcheck_angle[i] < 0.05 and mychain.slc_vtxcheck_angle[i] != -9999)
         # one_track_cut = not (mychain.slc_ntrack[i] == 0)
         quality_cut = mychain.slc_passed_min_track_quality[i]
-        if flashmatch_cut and broken_tracks_cut and quality_cut and dist_cut:
+
+        # print(mychain.slc_flsmatch_score[i])
+        # if flashmatch_cut and broken_tracks_cut and quality_cut and dist_cut and mychain.slc_flsmatch_score[i] > 0:
+        if quality_cut:
             return mychain.slc_flsmatch_score[i]
-    return -99999
+
+    return 0
 
 
 def fill_tree(chain, chain_numu, weight, tree, option=""):
@@ -255,9 +242,8 @@ def fill_tree(chain, chain_numu, weight, tree, option=""):
     for i in range(chain.GetEntries()):
         chain.GetEntry(i)
         chain_numu.GetEntry(i)
-        if chain.passed:
-            neutrino_vertex = [chain.vx, chain.vy, chain.vz]
 
+        if chain.passed:
             track_fidvol = True
             for i in range(chain.n_tracks):
                 track_start = [
@@ -268,8 +254,7 @@ def fill_tree(chain, chain_numu, weight, tree, option=""):
                     chain.track_end_x[i],
                     chain.track_end_y[i],
                     chain.track_end_z[i]]
-                track_fidvol = track_fidvol and is_active(
-                    track_start) and is_active(track_end)
+                track_fidvol = track_fidvol and is_fiducial(track_start) and is_fiducial(track_end)
 
             shower_fidvol = True
             for i in range(chain.n_showers):
@@ -277,9 +262,7 @@ def fill_tree(chain, chain_numu, weight, tree, option=""):
                     chain.shower_start_x[i],
                     chain.shower_start_y[i],
                     chain.shower_start_z[i]]
-                shower_fidvol = shower_fidvol and is_active(shower_start)
-
-            dedx = chain.shower_dEdx[choose_shower(chain)][2]
+                shower_fidvol = shower_fidvol and is_fiducial(shower_start)
 
             option_check = True
             event_weight = weight
@@ -290,10 +273,33 @@ def fill_tree(chain, chain_numu, weight, tree, option=""):
                 option_check = abs(chain.nu_pdg) == 12
 
             shower_id = choose_shower(chain)
-            dedx = chain.shower_dEdx[shower_id][2]
+            track_id = choose_track(chain)
 
-            if is_active(
-                    neutrino_vertex) and track_fidvol and shower_fidvol and option_check:
+            track_vertex = [
+                chain.track_start_x[track_id],
+                chain.track_start_y[track_id],
+                chain.track_start_z[track_id]]
+
+            shower_vertex = [
+                chain.shower_start_x[shower_id],
+                chain.shower_start_y[shower_id],
+                chain.shower_start_z[shower_id]]
+
+            neutrino_vertex = [chain.vx, chain.vy, chain.vz]
+
+            shower_vertex_d = math.sqrt(
+                sum([(s - n)**2 for s, n in zip(shower_vertex, neutrino_vertex)]))
+
+            track_vertex_d = math.sqrt(
+                sum([(t - n)**2 for t, n in zip(track_vertex, neutrino_vertex)]))
+
+            dedx = chain.shower_dEdx[shower_id][2] < 3.5 and chain.shower_dEdx[shower_id][2] > 1
+            openangle = math.degrees(chain.shower_open_angle[shower_id]) < 15 and math.degrees(chain.shower_open_angle[shower_id]) > 1
+            shower_angle = math.degrees(chain.shower_theta[shower_id]) < 80
+            track_distance = track_vertex_d < 5
+            shower_distance = shower_vertex_d < 7
+
+            if option_check and is_fiducial(neutrino_vertex) and shower_fidvol and track_fidvol:# and numu_score <  0.5 and dedx and openangle and shower_angle and track_distance and shower_distance:  # :
                 total_events += event_weight
                 fill_kin_branches(chain, chain_numu, event_weight, variables)
                 tree.Fill()
@@ -301,7 +307,7 @@ def fill_tree(chain, chain_numu, weight, tree, option=""):
     return total_events
 
 
-cosmic_mc = glob("cosmic_only/*/*.root")
+cosmic_mc = glob("cosmic_intime/*/*.root")
 
 # nue_cosmic = glob("nue_files_6_42_energy/*/*.root")
 # bnb_cosmic = glob("nu_files_6_42_energy/*/*.root")
@@ -309,11 +315,11 @@ cosmic_mc = glob("cosmic_only/*/*.root")
 # data_bnbext = glob("data_files_bnbext_6_42_energy/*/*.root")
 # data_ext_scaling_factor = 1.2640
 
-data_ext_scaling_factor = 1.2693
+data_ext_scaling_factor = 1.299
 nue_cosmic = glob("mc_nue_old/*/*.root")
-bnb_cosmic = glob("mc_bnb_nofidvol/*/*.root")
-data_bnb = glob("data_bnb_nofidvol/*/*.root")
-data_bnbext = glob("data_ext_nofidvol/*/*.root")
+bnb_cosmic = glob("mc_bnb_mcc83/*/*.root")
+data_bnb = glob("data_bnb_mcc83/*/*.root")
+data_bnbext = glob("data_ext_mcc83/*/*.root")
 
 chain_cosmic_mc = TChain("robertoana/pandoratree")
 chain_cosmic_mc_numu = TChain("UBXSec/tree")
@@ -336,7 +342,7 @@ chain_data_bnbext_numu = TChain("UBXSec/tree")
 
 for f in cosmic_mc:
     chain_cosmic_mc.Add(f)
-    chain_cosmic_mc_numu.Add(f)
+    #chain_cosmic_mc_numu.Add(f)
 
 for f in nue_cosmic:
     chain_nue.Add(f)
@@ -382,7 +388,7 @@ for i in range(chain_data_bnb_pot.GetEntries()):
 
 run_subrun_bnb.close()
 
-total_data_bnb_pot = 3.787e19
+total_data_bnb_pot = 4.758e19
 print("Total data POT BNB {0:.2e}".format(total_data_bnb_pot))
 
 run_subrun_ext = open("run_subrun_ext.txt", "w")
@@ -411,39 +417,40 @@ for n, b in variables.items():
 
 print("*** MC cosmic sample ***")
 total_cosmic_mc = fill_tree(chain_cosmic_mc, chain_cosmic_mc_numu, 1,
-                            cosmic_mc_tree)
+                            cosmic_mc_tree, "cosmic")
 print("MC cosmic {} events".format(total_cosmic_mc))
 
 print("*** MC BNB + cosmic sample ***")
+print("Weight {0:.2f}".format(total_pot / total_bnb_pot))
 total_mc = fill_tree(chain, chain_numu, total_pot / total_bnb_pot, mc_tree,
                      "bnb")
 print("MC {0:.0f} events".format(total_mc))
 
 print("*** MC nu_e + cosmic sample ***")
+print("Weight {0:.2f}".format(total_pot / total_nue_pot))
 total_nu_e = fill_tree(chain_nue, chain_nue_numu, total_pot / total_nue_pot,
                        mc_tree, "nue")
 print("MC nu_e {0:.0f} events".format(total_nu_e))
 
 print("*** Data BNB sample ***")
+print("Weight {0:.2f}".format(total_pot / total_data_bnb_pot))
 total_data_bnb = fill_tree(chain_data_bnb, chain_data_bnb_numu,
                            total_pot / total_data_bnb_pot, bnb_tree)
 print("Data BNB {0:.0f} events".format(total_data_bnb))
 
 print("*** Data EXT sample ***")
+print("Weight {0:.2f}".format(data_ext_scaling_factor * total_pot / total_data_bnb_pot))
 total_data_ext = fill_tree(
     chain_data_bnbext,
     chain_data_bnbext_numu,
-    data_ext_scaling_factor *
-    total_pot /
-    total_data_bnb_pot,
+    data_ext_scaling_factor * total_pot / total_data_bnb_pot,
     bnbext_tree)
 print("Data EXT {0:.0f} events".format(total_data_ext))
 
 bnb_ext = total_data_bnb - total_data_ext
 print("Data BNB-EXT {0:.0f} events".format(bnb_ext))
 
-print(
-    "Ratio (BNB-EXT)/MC {0:.2f}".format(bnb_ext / total_mc))
+print("Ratio (BNB-EXT)/MC {0:.2f}".format(bnb_ext / total_mc))
 
 cosmic_mc_file = TFile("cosmic_mc_file.root", "RECREATE")
 cosmic_mc_tree.Write()
