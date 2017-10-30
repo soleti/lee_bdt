7f473d47567733353e70f68b7abf4de4583997ff
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2017-10-29 21:35:55 -0400

    Measure best sigma

--- mc_bdt.py
+++ mc_bdt.py
@@ -4,7 +4,7 @@ import ROOT
 from array import array
 import math
 from bdt_common import bdt_cut, binning, labels, find_interaction, variables, spectators, sigmaCalc
-from bdt_common import interactions, x_start, x_end, y_start, y_end, z_start, z_end
+from bdt_common import interactions, x_start, x_end, y_start, y_end, z_start, z_end, manual_cuts
 
 fout = ROOT.TFile("test.root")
 
@@ -29,6 +29,7 @@ for name, var in spectators:
     ttrain.SetBranchAddress(name, var)
 
 variables_dict = dict(variables+spectators)
+bins = array("f", [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.8, 1])
 
 for i, n in enumerate(variables_dict.keys()):
     # h = ROOT.TH1F(n,labels[i],binning[i][0],binning[i][1],binning[i][2])
@@ -37,8 +38,11 @@ for i, n in enumerate(variables_dict.keys()):
     h_stack = ROOT.THStack("h_" + n, labels[n])
 
     for c in categories:
-        h = ROOT.TH1F("h_%s_%s" % (n, c), labels[n],
-                      binning[n][0], binning[n][1], binning[n][2])
+        if n != "reco_energy":
+            h = ROOT.TH1F("h_%s_%s" % (n, c), labels[n],
+                          binning[n][0], binning[n][1], binning[n][2])
+        else:
+            h = ROOT.TH1F("h_%s_%s" % (n, c), labels[n],len(bins)-1, bins)
         histos.append(h)
 
     h_stack.Add(histos[0])
@@ -86,55 +90,74 @@ h_lee = ROOT.TH1F("h_lee","",16, 0.2, 1)
 scaling = [6.0920944819073988, 3.6447414342239273, 3.2123920194399913, 2.6504659907742409, 3.2558450032216988, 2.5826310533377432, 2.6614353575699727, 1.4145769564088304, 1.0206172427887652, 0.9972444259255292, 0.79323702430381904, 0.63892043872491167, 0.61676413081900316, 0.3541651442224471, 0.28310400773433003, 0.94342108559739024]
 best_sigma = 0
 chosen_cut = 0
-h_chosen_lee = ROOT.TH1F("h_chosen_lee", "", 16, 0.2, 1)
-h_chosen_bkg = ROOT.TH1F("h_chosen_bkg", "", 16, 0.2, 1)
+h_chosen_lee = ROOT.TH1F("h_chosen_lee", "", len(bins) - 1, bins)
+h_chosen_bkg = ROOT.TH1F("h_chosen_bkg", "", len(bins) - 1, bins)
 
-for i in range(200):
-    bdt_cut = 0.2+i*0.004
-    h_bkg.Reset()
-    h_sig.Reset()
-    h_lee.Reset()
-    for i in range(tout.GetEntries()):
-        tout.GetEntry(i)
-        if tout.BDT > bdt_cut:
+scaling = [6.0920944819073988, 3.6447414342239273, 3.2123920194399913,
+           2.6504659907742409, 3.2558450032216988, 2.5826310533377432,
+           2, 1, 1, 1, 1]
+measure_sigma = False
 
-            if tout.category == 2:
-                h_sig.Fill(tout.reco_energy, tout.event_weight)
-            else:
-                h_bkg.Fill(tout.reco_energy, tout.event_weight)
+if measure_sigma:
+    for i in range(200):
+        cut = 0.2+i*0.004
+        h_bkg.Reset()
+        h_sig.Reset()
+        h_lee.Reset()
+        for i in range(tout.GetEntries()):
+            tout.GetEntry(i)
 
-    for i in range(ttrain.GetEntries()):
-        ttrain.GetEntry(i)
-        if ttrain.BDT > bdt_cut:
-            if ttrain.category == 2:
-                h_sig.Fill(ttrain.reco_energy, ttrain.event_weight)
-            else:
-                h_bkg.Fill(ttrain.reco_energy, ttrain.event_weight)
+            shower_energy = tout.shower_energy > 0.2
+            dedx = 1.4 < tout.dedx < 3
+            if tout.BDT > cut:
 
-    for i in range(len(scaling)):
-        if scaling[i] - 1 > 0:
-            h_lee.SetBinContent(i+1, h_sig.GetBinContent(i+1)*(scaling[i] - 1))
+                if tout.category == 2:
+                    h_sig.Fill(tout.reco_energy, tout.event_weight)
+                else:
+                    h_bkg.Fill(tout.reco_energy, tout.event_weight)
 
-    h_bkg.Add(h_sig)
-    f_cosmic_in_time = ROOT.TFile("bdt_cosmic/cut%.3f.root" % bdt_cut)
-    h_cosmic_in_time = f_cosmic_in_time.Get("h_reco")
-    h_bkg.Add(h_cosmic_in_time)
+        for i in range(ttrain.GetEntries()):
+            ttrain.GetEntry(i)
+            shower_energy = ttrain.shower_energy > 0.2
+            dedx = 1.4 < ttrain.dedx < 3
+            if ttrain.BDT > cut:
+                if ttrain.category == 2:
+                    h_sig.Fill(ttrain.reco_energy, ttrain.event_weight)
+                else:
+                    h_bkg.Fill(ttrain.reco_energy, ttrain.event_weight)
 
-    if sigmaCalc(h_lee, h_bkg) > best_sigma:
-        best_sigma = sigmaCalc(h_lee, h_bkg)
-        h_chosen_lee = h_lee.Clone()
-        h_chosen_bkg = h_bkg.Clone()
-        chosen_cut = bdt_cut
-    f_cosmic_in_time.Close()
-print(best_sigma, chosen_cut)
+
+        for i in range(len(scaling)):
+            if scaling[i] - 1 > 0:
+                h_lee.SetBinContent(i+1, h_sig.GetBinContent(i+1)*(scaling[i] - 1))
+
+        h_bkg.Add(h_sig)
+        f_cosmic_in_time = ROOT.TFile("bdt_cosmic/cut%.3f.root" % cut)
+        h_cosmic_in_time = f_cosmic_in_time.Get("h_reco")
+        h_bkg.Add(h_cosmic_in_time)
+
+        if sigmaCalc(h_lee, h_bkg) > best_sigma:
+            best_sigma = sigmaCalc(h_lee, h_bkg)
+            h_chosen_lee = h_lee.Clone()
+            h_chosen_bkg = h_bkg.Clone()
+            chosen_cut = cut
+        f_cosmic_in_time.Close()
+    print(best_sigma, chosen_cut)
 
 for i in range(ttrain.GetEntries()):
     ttrain.GetEntry(i)
     category = int(ttrain.category)
     h_bdts[category].Fill(ttrain.BDT, ttrain.event_weight)
 
-    if ttrain.BDT > chosen_cut:
+
+    if ttrain.BDT > bdt_cut and manual_cuts(ttrain):
+        if ttrain.category == 2 and 0 < ttrain.reco_energy < 2:
+            nue_bdt += ttrain.event_weight
+
         for name, var in variables:
+            histo_dict[name][category].Fill(var[0], ttrain.event_weight)
+
+        for name, var in spectators:
             histo_dict[name][category].Fill(var[0], ttrain.event_weight)
 
         if ttrain.reco_energy > 0.2 and (ttrain.category == 3 or ttrain.category == 4):
@@ -150,10 +173,10 @@ for i in range(tout.GetEntries()):
     if tout.category == 2:
         nue_selected += 2
 
-    if tout.BDT > chosen_cut:
+    if tout.BDT > bdt_cut and manual_cuts(tout):
 
-        if tout.category == 2:
-            nue_bdt += 2
+        if tout.category == 2 and 0 < tout.reco_energy < 2:
+            nue_bdt += tout.event_weight
 
         # Store interaction types of background events
         if tout.reco_energy > 0.2 and (tout.category == 3 or tout.category == 4):
