bea1b46a805dc431446b96abf3f46b0f0e4a0f3d
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2018-02-20 13:31:45 +0000

    Proton energy study

---
+++ proton_energy.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import ROOT
+from array import array
+
+ARGON_DENSITY = 1.396
+
+def length2energy(length):
+    """Return the energy (in GeV) of the track based on its length and
+    the stopping power in the argon"""
+
+    with open("proton_range.txt", "r") as table_file:
+        lines = table_file.readlines()
+        a_range = array("f", [])
+        a_energy = array("f", [])
+
+        for line in lines:
+            values = line.split()
+            a_range.append(float(values[1]))
+            a_energy.append(float(values[0]))
+        
+        g_proton_range = ROOT.TGraph(len(lines), a_range, a_energy)
+        return g_proton_range.Eval(length * ARGON_DENSITY, 0, "S") / 1000
+    
