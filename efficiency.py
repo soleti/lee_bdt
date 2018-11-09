80c00f1e37aff06f01360e022d61540fa64667bf
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2018-11-09 12:21:59 -0500

    Efficiency improvements

--- efficiency.py
+++ efficiency.py
@@ -1,18 +1,17 @@
 #!/usr/local/bin/python3
 
 import math
-import ROOT
-import sys
-from array import array
+import statistics
 from glob import glob
 
-from settings import printProgressBar, distance, binning, labels
-from settings import is_fiducial, is_active, variables, spectators
+import ROOT
+
+from settings import printProgressBar, binning, labels
+from settings import is_fiducial, variables, spectators
 from settings import pre_cuts, load_bdt, apply_cuts, fill_kin_branches
-from settings import BINS, BINS, BDT_TYPES, N_UNI, ELECTRON_THRESHOLD, PROTON_THRESHOLD
+from settings import N_UNI, ELECTRON_THRESHOLD, PROTON_THRESHOLD
 from settings import PROTON_MASS, ELECTRON_MASS
 
-from proton_energy import length2energy
 
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptFit(0)
@@ -21,22 +20,80 @@ ROOT.gStyle.SetNumberContours(99)
 
 
 class Efficiency:
-    def __init__(self, tot, selected={}, var="", pot=0, weights=[]):
+    def __init__(self, tot, selected={}, var="", pot=0, total_sys_err=0, sys_err=[]):
         self.tot = tot
         self.pot = pot
         self.selected = selected
         self.efficiency = {}
         self.efficiency_err = {}
         self.variable = var
-        self.weights = weights
-        for s in selected:
-            eff_value = selected[s].Integral()/tot.Integral()
-            self.efficiency[s] = eff_value
-            self.efficiency_err[s] = 1./tot.Integral() * math.sqrt(selected[s].Integral()*(1-eff_value))
+        self.sys_err = sys_err
+        self.total_sys_err = total_sys_err
+        for sel in selected:
+            eff_value = selected[sel].Integral()/tot.Integral()
+            self.efficiency[sel] = eff_value
+            self.efficiency_err[sel] = 1./tot.Integral() * math.sqrt(selected[sel].Integral()*(1-eff_value))
 
-    def tefficiency(self, num, uni=0):
+    def tefficiency(self, num):
         teff = ROOT.TEfficiency(self.selected[num], self.tot)
         return teff
+
+    def tefficiency_err(self, num):
+        teff = self.tefficiency(num)
+        teff_sys = self.selected[num].Clone()
+        teff_sys.Divide(self.tot)
+
+        for i_bin in range(1, self.selected[num].GetNbinsX()+1):
+            stat_error = teff.GetEfficiencyErrorLow(i_bin) + teff.GetEfficiencyErrorUp(i_bin)
+            teff_sys.SetBinError(i_bin, math.sqrt(stat_error**2 + self.sys_err[i_bin - 1]**2))
+
+        return teff_sys
+
+
+    def draw(self, systematics=True):
+        c_eff_sys = ROOT.TCanvas("c_eff_sys_%s" % self.tot.GetName())
+        eff_stat = self.tefficiency("passed")
+        eff_sys = self.tefficiency_err("passed")
+        eff_stat.SetLineWidth(2)
+        eff_stat.Draw("ap")
+        eff_sys.SetLineColor(1)
+
+        if systematics:
+            eff_sys.Draw("e same")
+            eff_stat.Draw("p same")
+
+        l_eff = ROOT.TLegend(0.097, 0.854, 0.9, 0.9)
+
+        if systematics:
+            l_eff.AddEntry(eff_stat,
+                        "Selection efficiency: (%.1f #pm %.1f (stat) #pm %.1f (sys)) %%"
+                        % (self.efficiency["passed"] * 100, self.efficiency_err["passed"] * 100, self.total_sys_err * 100),
+                        "le")
+        else:
+            l_eff.AddEntry(eff_stat,
+                           "Selection efficiency: (%.1f #pm %.1f) %%"
+                           % (self.efficiency["passed"] * 100, self.efficiency_err["passed"] * 100),
+                           "le")
+        l_eff.Draw()
+        c_eff_sys.SetTopMargin(0.18)
+
+        return c_eff_sys, eff_stat, eff_sys, l_eff
+
+
+def draw_true(histo):
+    tot = histo.Clone()
+    tot.SetLineColor(ROOT.TColor.GetColor("#62b570"))
+    tot.SetLineWidth(3)
+    leg = ROOT.TLegend(0.097, 0.854, 0.9, 0.9)
+    leg.SetNColumns(2)
+    leg.AddEntry(tot,
+                 "Generated #nu_{e} CC0#pi-Np: %.1f events" % tot.Integral(),
+                 "f")
+    c = ROOT.TCanvas("c_tot")
+    tot.Draw("hist")
+    c.SetTopMargin(0.16)
+
+    return c, tot, leg
 
 
 def stacked(eff, h_stack, colors):
@@ -53,8 +110,7 @@ def stacked(eff, h_stack, colors):
         h_cat.SetLineWidth(1)
         if cat == "passed":
             h_cat.SetLineWidth(3)
-            h_cat.SetLineColor(ROOT.TColor.GetColor(1))
-            h_cat.SetFillColor(ROOT.TColor.GetColor(0))
+            h_cat.SetLineColor(1)
         h_cats.append(h_cat)
         leg.AddEntry(h_cat, "%s: %.1f%%" % (h_cat.GetName(), e*100), "f")
         h_stack.Add(h_cat)
@@ -111,7 +167,7 @@ def check_cc0pinp(chain_nue):
 
     for i, energy in enumerate(chain_nue.nu_daughters_E):
         if chain_nue.nu_daughters_pdg[i] == 2212:
-            proton_energy += energy - 0.938
+            proton_energy += energy - PROTON_MASS
 
             p_vertex = [chain_nue.nu_daughters_start_v[i][0],
                         chain_nue.nu_daughters_start_v[i][1],
@@ -121,10 +177,10 @@ def check_cc0pinp(chain_nue):
                      chain_nue.nu_daughters_end_v[i][1],
                      chain_nue.nu_daughters_end_v[i][2]]
 
-            if energy - 0.938 > PROTON_THRESHOLD:
+            if energy - PROTON_MASS > PROTON_THRESHOLD:
                 protons += 1
             if not is_fiducial(p_vertex) or not is_fiducial(p_end):
-                proton = 0
+                protons = 0
                 break
 
         if chain_nue.nu_daughters_pdg[i] == 11:
@@ -133,7 +189,7 @@ def check_cc0pinp(chain_nue):
                         chain_nue.nu_daughters_end_v[i][1],
                         chain_nue.nu_daughters_end_v[i][2]]
 
-            if energy - 0.51e-3 > ELECTRON_THRESHOLD and is_fiducial(e_vertex):
+            if energy - ELECTRON_MASS > ELECTRON_THRESHOLD and is_fiducial(e_vertex):
                 electrons += 1
 
         if chain_nue.nu_daughters_pdg[i] == 22:
@@ -156,19 +212,44 @@ def lepton_angles(chain_filter):
             py = chain_filter.nu_daughters_p[i][1]
             pz = chain_filter.nu_daughters_p[i][2]
             theta = math.degrees(math.acos(pz/math.sqrt(px**2+py**2+pz**2)))
-            phi = math.degrees(math.atan2(py,px))
+            phi = math.degrees(math.atan2(py, px))
             break
 
     return theta, phi
 
 
-def efficiency(files_path, eff_variables=[], scale=1):
+def eff_total_sys_error(h_tot_sys, h_passed_sys):
+    ratios = []
+    for u in range(N_UNI):
+        ratio = h_passed_sys[u].Integral()/h_tot_sys[u].Integral()
+        ratios.append(ratio)
+
+    return statistics.stdev(ratios)
+
+
+def eff_sys_error(h_tot_sys, h_passed_sys):
+    sys_error = []
+
+    for i_bin in range(1, h_tot_sys[0].GetNbinsX()+1):
+        err = []
+        for u in range(N_UNI):
+            if h_tot_sys[u].GetBinContent(i_bin):
+                err.append(h_passed_sys[u].GetBinContent(i_bin)/h_tot_sys[u].GetBinContent(i_bin))
+
+        if len(err) > 1:
+            sys_error.append(statistics.stdev(err))
+        else:
+            sys_error.append(0)
+
+    return sys_error
+
+
+def efficiency(files_path, eff_variables=[], systematics=False, scale=1):
     nue_cosmic = glob(files_path+"/*.root")
     chain_nue = ROOT.TChain("robertoana/pandoratree")
     chain_filter = ROOT.TChain("nueFilter/filtertree")
     chain_pot = ROOT.TChain("nueFilter/pot")
 
-    # print(nue_cosmic)
     for f in nue_cosmic:
         chain_filter.Add(f)
         chain_nue.Add(f)
@@ -189,6 +270,7 @@ def efficiency(files_path, eff_variables=[], scale=1):
                   "cosmic selected", "1 shower",  "no showers",  "no flash", "no data products"]
 
     h_tot = {}
+    h_tot_sys = {}
 
     for v in eff_variables:
         h_tot[v] = ROOT.TH1F("h_tot_%s" % v,
@@ -196,8 +278,17 @@ def efficiency(files_path, eff_variables=[], scale=1):
                              binning[v][0],
                              binning[v][1],
                              binning[v][2])
+        h_tot_uni = []
+        for u in range(N_UNI):
+            h_tot_uni.append(ROOT.TH1F("h_tot_%s_%i" % (v, u),
+                                       labels[v],
+                                       binning[v][0],
+                                       binning[v][1],
+                                       binning[v][2]))
+        h_tot_sys[v] = h_tot_uni
 
     h_selected = {}
+    h_passed_sys = {}
     for v in eff_variables:
         h_sel = {}
         for c in categories:
@@ -207,6 +298,15 @@ def efficiency(files_path, eff_variables=[], scale=1):
                                  binning[v][1],
                                  binning[v][2])
         h_selected[v] = h_sel
+
+        h_passed_uni = []
+        for u in range(N_UNI):
+            h_passed_uni.append(ROOT.TH1F("h_tot_%s_%i" % (v, u),
+                                          labels[v],
+                                          binning[v][0],
+                                          binning[v][1],
+                                          binning[v][2]))
+        h_passed_sys[v] = h_passed_uni
 
     i_nue = -1
     eff_vars = {}
@@ -238,12 +338,18 @@ def efficiency(files_path, eff_variables=[], scale=1):
         eff_vars["energy"] = chain_filter.nu_energy
         eff_vars["theta"], eff_vars["phi"] = lepton_angles(chain_filter)
 
-        flux_weights = [1]  # [1]*len(chain_nue.flux_weights[0])
-        # for fl in chain_nue.flux_weights:
-        #     flux_weights = [a*b for a, b in zip(flux_weights, fl)]
+        if systematics:
+            flux_weights = [1]*len(chain_filter.flux_weights[0])
+            for fl in chain_filter.flux_weights:
+                flux_weights = [a*b for a, b in zip(flux_weights, fl)]
+            genie_weights = chain_filter.genie_weights[0]
         weight = chain_filter.bnbweight
+
         for v in eff_variables:
             h_tot[v].Fill(eff_vars[v], weight)
+
+            for u in range(N_UNI):
+                h_tot_sys[v][u].Fill(eff_vars[v], weight * flux_weights[u] * genie_weights[u])
 
             if not chain_filter.passed:
                 if chain_filter.selection_result == 2:
@@ -285,6 +391,8 @@ def efficiency(files_path, eff_variables=[], scale=1):
                 continue
 
             h_selected[v]["passed"].Fill(eff_vars[v], weight)
+            for u in range(N_UNI):
+                h_passed_sys[v][u].Fill(eff_vars[v], weight * flux_weights[u] * genie_weights[u])
 
             # bdt_values = {}
             # for bdt_name in BDT_TYPES:
@@ -297,6 +405,8 @@ def efficiency(files_path, eff_variables=[], scale=1):
 
     eff_objects = {}
     for v in eff_variables:
-        eff_objects[v] = Efficiency(h_tot[v], h_selected[v], v, total_pot)
-    return eff_objects
+        sys_err = eff_sys_error(h_tot_sys[v], h_passed_sys[v])
+        total_sys_err = eff_total_sys_error(h_tot_sys[v], h_passed_sys[v])
+        eff_objects[v] = Efficiency(h_tot[v], h_selected[v], v, total_pot, total_sys_err, sys_err)
 
+    return eff_objects
